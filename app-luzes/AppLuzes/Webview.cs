729578f03b8219499b0b40890a2def2913be480f
João Vitor Fernandes Canuto <joao678@gmail.com>
2022-06-15 23:10:46 -0300

    first commit

---
+++ app-luzes/AppLuzes/Webview.cs
@@ -0,0 +1,20 @@
+﻿using SharpWebview;
+using SharpWebview.Content;
+using System;
+
+namespace webview_test
+{
+    public class Webview
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var webview = new SharpWebview.Webview(true,true);
+            webview.SetTitle("Teste")
+                .SetSize(1024, 768, WebviewHint.None)
+                .SetSize(800, 600, WebviewHint.Min)
+                .Navigate(new HostedContent());
+            webview.Run();
+        }
+    }
+}
